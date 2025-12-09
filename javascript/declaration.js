494c982047694c786b71d303aa79386ddde6c488
aadithya2007 <aadithya2807@gmail.com>
2025-12-09 15:37:26 +0530

    day5

--- javascript/declaration.js
+++ javascript/declaration.js
@@ -9,7 +9,7 @@ var a=10;
 console.log("a",a);
 console.log("b",b);
 //problem of var keyword is that it can be reinitialized or redeclared
-console.log(c);//reference error
+//console.log(c);//reference error
 let c=10;
 let d=a;
 a=20;
@@ -19,3 +19,25 @@ console.log("d",d);
 //to avoid er initialization we use const
 const pi=3.14;
 //pi=3.15; //error
+
+//nested object
+let person={
+    fname:"Harry",
+    lname:"potter",
+    age:10,
+    address:{
+        city:"chennai",
+        pincode:"641002",
+    },
+}
+console.log(person);
+console.log(person.lname);
+console.log(person.address.city);
+
+//destructuring
+const {fname,lname,age,address}=person;
+const {city,pincode}=address;
+console.log(fname);
+console.log(city);
+console.log(pincode);
+
