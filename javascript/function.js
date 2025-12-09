a4013dc5d0a79d0da3054fba6dba3fdd554839ba
aadithya2007 <aadithya2807@gmail.com>
2025-12-09 15:10:17 +0530

    day5

--- javascript/function.js
+++ javascript/function.js
@@ -10,3 +10,15 @@ var product=function(a,b,c){
 }
 console.log(product(2,3,4));
 console.log("_____________");
+//IIFE function-Immediate invoked func expression
+(function(a,b){
+    console.log(b-a);
+})(10,20);
+console.log("_____________");
+
+//arrow function
+var divide=(a,b)=>{
+    return a/b;
+}
+console.log(divide(20,5));
+console.log("_____________");
