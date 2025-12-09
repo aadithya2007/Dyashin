fb4ed15bf692966858b813c4aab1d9a996b766c5
aadithya2007 <aadithya2807@gmail.com>
2025-12-09 15:04:48 +0530

    day5

---
+++ javascript/function.js
@@ -0,0 +1,12 @@
+//named function
+function add(a,b){
+ return a+b;
+}
+console.log(add(10,20));
+console.log("_____________");
+//anonymous function
+var product=function(a,b,c){
+    return a*b*c;
+}
+console.log(product(2,3,4));
+console.log("_____________");
