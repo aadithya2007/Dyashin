a9e8802dd6a90b7d0ea9203b58d73b7296f3a85d
aadithya2007 <aadithya2807@gmail.com>
2025-12-09 15:46:09 +0530

    day5

---
+++ javascript/copy.js
@@ -0,0 +1,29 @@
+let person={
+    fname:"Harry",
+    lname:"potter",
+    age:10,
+    address:{
+        city:"chennai",
+        pincode:"641002",
+    },
+}
+let personCopy=person;//normal copy
+console.log(person);
+console.log(personCopy);
+person.age=34;
+console.log("After modifying age");
+console.log(person);
+console.log(personCopy);
+
+let personCopy2={...person}//using.. spread operator (shallow copy)
+console.log(person);
+console.log(personCopy2);
+person.address.city="mumbai";
+console.log("After modifying city");
+console.log(person);
+console.log(personCopy2);
+
+
+let personCopy3=JSON.parse(JSON.stringify(person));//deep copy
+console.log(person);
+console.log(personCopy3);
