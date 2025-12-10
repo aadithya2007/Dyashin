3845ce7359e1b8830d67a442d6e32d3fabc42835
aadithya2007 <aadithya2807@gmail.com>
2025-12-10 15:05:47 +0530

    day6

--- javascript/strings.js
+++ javascript/strings.js
@@ -1,2 +1,23 @@
 let str1="hello World";
 console.log(str1.length);
+console.log(str1.at(-1));
+console.log(str1.charAt(2));
+console.log(str1.indexOf("o"));
+console.log(str1.lastIndexOf("o"));
+console.log(str1.slice(6,11));
+console.log(str1.substring(5,11));
+console.log(str1.startsWith("he"));
+console.log(str1.endsWith("ld"));
+console.log(str1.includes("lo Wo"));
+console.log(str1.replace("o","O"));
+console.log(str1.toUpperCase());
+console.log(str1.replaceAll("o","O"));
+console.log(str1.concat(" hello"));
+let str2="    hello world      "
+console.log(str2);
+console.log(str2.trim());
+console.log(str2.trimStart());
+console.log(str2.trimEnd());
+console.log(str1.repeat(5));
+console.log(str1.split(""));
+
